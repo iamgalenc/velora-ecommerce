0401d7a35b03dfd085ce5227ff496288f82d0e44
iamgalenc <estgravity@gmail.com>
2025-09-30 12:06:11 +0700

    Initial

---
+++ config.py
@@ -0,0 +1,18 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
+    CA_CERT_PATH = os.getenv('CA_CERT_PATH')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
